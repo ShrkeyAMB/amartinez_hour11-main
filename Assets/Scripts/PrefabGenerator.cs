3c0922863d66b9a4596786aa8bc608ea572c305e
ShrkeyAMB <mymshrkeymym@gmail.com>
2023-04-20 22:04:28 -0700

    BetterCamaraAngle

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -10,33 +10,7 @@ public class PrefabGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int Lamps = 10;
-
-        do
-        {
-            Instantiate(prefab, new Vector3(-1, 0, 5), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 15), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 25), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 35), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 45), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 55), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 65), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 75), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 85), transform.rotation);
-            Lamps--;
-            Instantiate(prefab, new Vector3(-1, 0, 95), transform.rotation);
-            Lamps--;
-
-        }
-        while (Lamps > 0);
+        
         
 
      
@@ -48,13 +22,41 @@ public class PrefabGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int Lamps = 10;
+
         if (Input.GetKeyDown(KeyCode.B))
         {
             Instantiate(prefab);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(prefab, transform.position, transform.rotation);
+            
+
+            do
+            {
+                Instantiate(prefab, new Vector3(-1, 0, 5), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 15), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 25), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 35), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 45), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 55), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 65), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 75), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 85), transform.rotation);
+                Lamps--;
+                Instantiate(prefab, new Vector3(-1, 0, 95), transform.rotation);
+                Lamps--;
+
+            }
+            while (Lamps > 0);
 
         }
     }
